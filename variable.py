b281dc53fea48971f5f16e371f252b1f6bb793a9
Sapana Dahal <dsapana370@gmail.com>
2025-08-28 14:38:42 +0545

    project added

---
+++ variable.py
@@ -0,0 +1,38 @@
+a = 21
+print(a)
+
+b = "Sapana"
+print(b)
+c = 5.50
+print(c)
+
+print("type of a is a ",type(a))
+print("type of a is a ",type(b))
+print("type of a is a ",type(c))
+
+# List
+
+fruits = ["apple", "banana", "mango"]
+print(fruits[0])   # apple
+
+# Tuple
+colors = ("red", "green", "blue")
+print(colors[1])   # green
+
+# dictonary
+
+student = {"name": "Sapana", "age": 21}
+print(student["name"])   # Sapana
+# Super simple rule to remember:
+
+# List → [ ] changeable
+
+# Tuple → ( ) unchangeable
+
+# Dictionary → { } key:value
+
+print(4 + 4)    # Addition → 8
+print(4 - 4)    # Subtraction → 0
+print(4 * 40)   # Multiplication → 160
+print(4 / 40)   # Division → 0.1
+print(5** 8)
