005900e66f16d2a31217708faa46bd7999a9c0b6
Sapana Dahal <sapanaa1206@gmail.com>
2025-09-05 15:29:57 +0545

    New file added

--- Loop.py
+++ Loop.py
@@ -1,8 +1,17 @@
-# count = 1
-# while count <= 4:
-#     print(count)
-#     count = count  +1
+count = 1
+while count <= 4:
+    print(count)
+    count = count  +1
+
+
 count = 5
 while count >= 0:
     print(count)
     count -= 1
+    
+    
+while  (i<=5): 
+    i = int (input("Enter the number"))
+    print(i)
+    
+    # print("Done with the loop")   
---
+++ numberguessing.py
@@ -0,0 +1,42 @@
+# import random
+
+# secret = random.randint(1, 20)
+
+# print("Welcome to Guess the Number Game! 🎲")
+# print("I'm thinking of a number between 1 and 20.")
+
+# while True:
+#     guess = int(input("Enter your guess: "))
+
+#     if guess == secret:
+#         print("🎉 Correct! You guessed the number.")
+#         break
+#     elif guess < secret:
+#         print("Too low! Try again.")
+#     else:
+#         print("Too high! Try again.")
+
+
+import random
+
+# Choices
+choices = ["rock", "paper", "scissors"]
+
+# Player input
+player = input("Enter rock, paper, or scissors: ").lower()
+
+# Computer random choice
+computer = random.choice(choices)
+
+print("You chose:", player)
+print("Computer chose:", computer)
+
+# Decide winner
+if player == computer:
+    print("It's a tie!")
+elif (player == "rock" and computer == "scissors") or \
+     (player == "scissors" and computer == "paper") or \
+     (player == "paper" and computer == "rock"):
+    print("You win! Yeahhhh!")
+else:
+    print("You lose! so Sad ")
