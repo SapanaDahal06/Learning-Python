12616825a49190a66fb2c25697f5df333490ec85
Sapana Dahal <dsapana370@gmail.com>
2025-09-05 15:29:57 +0545

    New file added

---
+++ breakstatement.py
@@ -0,0 +1,21 @@
+# for i in range(15):
+#  if(i==10):
+#   break
+#  print("5 X", i+1, "=", 5*(i+1))
+# print("Until this")
+
+
+# for s in range(15):
+#     if(s==12):
+#         break
+#     print("8 X ",s+1,"=" ,8*(s+1) )
+    
+# print("until this")
+    
+    #While loop 
+for j in range(10):
+    if j == 8:
+        print("Skip the iteration")
+        continue
+    print("9 X", j, "=", 9*j)
+ 
---
+++ forloop.py
@@ -0,0 +1,26 @@
+language = 'python' #Sequence
+for x in language:
+    print(x)
+    #For a list
+color = ["Red", "Yello", "White"]
+for x in color:
+    print(x)
+ 
+name  = ["Srijana", "Sandesh", "Sapana","Samjhana"]
+for x in name:
+    print(x)  
+    
+fruit = ["Apple","Bluebery","Mango","licchi"]
+for fruit in fruit:
+     print(fruit)
+     
+  # Range in python 
+for k in range(10):
+    print(k+1)
+    
+for l in range(50):
+    print(l+1)
+    
+  
+     
+     
