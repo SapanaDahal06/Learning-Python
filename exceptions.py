721416cc570e377258b98275134b6728393b238d
Sapana Dahal <sapanaa1206@gmail.com>
2026-02-04 18:55:24 +0545

    Add improved exception handling example

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,16 @@
-a = int(input("Enter the number: "))
-print(f"Multiplication table of {a} is:")
+try:
+    a = int(input("Enter the number: "))
+    print(f"Multiplication table of {a} is:")
+    
+    for i in range(1, 11):
+        print(f"{a} X {i} = {a * i}")
 
-for i in range(1, 11):
-    print(f"{a} X {i} = {a * i}")
+except ValueError:
+    print("Please enter a valid number!")
+
+except Exception as e:
+    print(f"Sorry, an unexpected error occurred: {e}")
+
+finally:
+    print("Some line of code")
+    print("End of the code.")
